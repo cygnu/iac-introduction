93b2dfd69b1586006b1f1101d6c27e71a4b07c62
cygnu <crucia8@icloud.com>
2022-03-06 14:11:24 +0900

    feat: Configure bucket policy settings

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.this.arn]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::my-s3-bucket/",
+    ]
+  }
+}
+
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.14.1"
@@ -5,6 +22,8 @@ module "s3-bucket" {
 
   bucket = "my-s3-bucket"
   acl    = "public-read"
+
+  policy = data.aws_iam_policy_document.bucket_policy.json
 
   website = {
     index_document = "index.html"
